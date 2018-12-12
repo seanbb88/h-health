1e7c646c674732d00deb550d5710a55e85f45770
Sean Brown <sbbrown35@gmail.com>
2018-12-11 21:13:11 -0600

    updated css
    updated css

--- client/src/component/SavingsCard.js
+++ client/src/component/SavingsCard.js
@@ -7,10 +7,23 @@ class SavingsCard extends Component {
         return (
             <div className="savings-card">
                 <div className="savings-card-line">
-                    <LineChart data={{ "2017-05-13": 2, "2017-05-14": 5 }} />
+                    <LineChart 
+                        data={{ "2018-01-08": 2,
+                                "2018-01-16": 5,
+                                "2018-04-05": 2,
+                                "2018-06-14": 5,
+                                "2018-08-01": 2,
+                                "2018-08-08": 5,
+                                "2018-10-13": 2,
+                                "2018-11-22": 5
+                                 }}
+                        width="500px"
+                        height="300px"
+                        prefix="$"
+                     />
                 </div>
                 <div className="savings-card-bar">
-                    <ColumnChart data={[["In-Network", 40], ["Out-Network", 20]]} />
+                    {/* <ColumnChart data={[["In-Network", 40], ["Out-Network", 20]]} /> */}
                 </div>
             </div>
         )
